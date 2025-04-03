f55ccb424ba0eedc5eccc64aa3e1f8b5880fd327
Swarnil Kokulwar <swarnil.sk@gmail.com>
2025-04-03 23:48:34 +0530

    nothing much

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,5 +1,12 @@
-
 export function TokenLaunchpad() {
+
+    function createToken() {
+        const name = document.getElementById('name').value;
+        const symbol = document.getElementById('symbol').value;
+        const image = document.getElementById('image').value;
+        const initialSupply = document.getElementById('initialSupply').value;
+    }
+
     return  <div style={{
         height: '100vh',
         display: 'flex',
@@ -12,6 +19,6 @@ export function TokenLaunchpad() {
         <input className='inputText' type='text' placeholder='Symbol'></input> <br />
         <input className='inputText' type='text' placeholder='Image URL'></input> <br />
         <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <button onClick={createToken} className='btn'>Create a token</button>
     </div>
 }
